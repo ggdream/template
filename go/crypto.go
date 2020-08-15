1d5ce715492105503e0654669ef5804c75b78796
ggdream <gdream@yeah.net>
2020-08-15 23:43:51 +0800

    golang encv0.0.2

--- go/crypto.go
+++ go/crypto.go
@@ -9,15 +9,15 @@ import (
 	"github.com/ggdream/crypto/rsa"
 )
 
-func Decrypt(data interface{}, privateKey *rsa.PrivateKey) []byte {
+func Decrypt(data interface{}) []byte {
 	var key		[]byte
 	var text	[]byte
 
-	if key = rsa.Decrypt(crypto.DeBase64(data.EncKey), privateKey); key == nil{
+	if key = rsa.Decrypt(crypto.DeBase64(data.EncKey), config.PrivateKey); key == nil{
 		return nil
 	}
 
-	if text = aes.Decrypt(crypto.DeBase64(string(aes.Decrypt(crypto.DeBase64(data.EncText), config.IV, config.IV))), config.IV, config.IV); text == nil{
+	if text = aes.Decrypt(crypto.DeBase64(string(aes.Decrypt(crypto.DeBase64(data.EncText), key, config.IV))), config.IV, config.IV); text == nil{
 		return nil
 	}
 
